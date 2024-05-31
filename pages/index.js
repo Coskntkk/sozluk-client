8cf242ff1c065b9448df1d5d13defcb4a105ed30
Coşkun Atak <coskntkk@gmail.com>
2024-05-31 13:09:29 +0300

    title

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <header class="bg-blue-700 text-white p-4">
       <div class="container mx-auto flex justify-between items-center">
-        <h1 class="text-2xl font-bold">Ekşi Sözlük</h1>
+        <h1 class="text-2xl font-bold">Sözlük</h1>
         <nav>
           <ul class="flex space-x-4">
             <li><a href="#" class="hover:bg-blue-600 p-2 rounded">Anasayfa</a></li>
